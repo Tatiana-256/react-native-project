94022c114af3b4ac020184546ed60f4fa5b32b5b
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-08-05 17:52:23 +0300

    rename folder

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {NavBar} from "./conponents/Header";
-import {AddToDo} from "./conponents/AddToDo";
-import {Task} from "./conponents/Tasks";
+import {NavBar} from "./components/Header";
+import {AddToDo} from "./components/AddToDo";
+import {Task} from "./components/Tasks";
 
 export default function App() {
     const [toDo, setToDo] = useState([{id: '212', task: 'sd'}])
